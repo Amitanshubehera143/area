92c3b1a8ca5161a5aa8b20ee312405c3b3360625
Amitanshubehera143 <24mca.amitanshubehera@gietbbsr.edu.in>
2025-03-22 14:07:58 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,13 +1,13 @@
-r=float(input("Enter radious of circle:"))
+r=float(input("Enter radius of circle:"))
 pie=3.14
 cp=2*pie*r
 ca=pie*r*r
 print("The parameter of circle:",cp,"\narea of circle:",ca)
 l=float(input("Enter the length of rectangle:"))
-b=float(input("Enter the breath of rectangle:"))
+b=float(input("Enter the bredth of rectangle:"))
 rp=2*(l+b)
 ra=l*b
 print("The parameter of the rectangle",rp,"\nThe area of rectangle:",ra)
-s=int(input("enter the one side of a sqare:"))
-print("parameter of sqare",4*s,"area of the sqare",s*s)
+s=int(input("enter the one side of a square:"))
+print("parameter of sqare",4*s,"area of the square",s*s)
       
